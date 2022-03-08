b929bd87815d36e894aae5d73be1da1ee36298ff
CircleCI <no-reply@build.com>
2022-03-08 13:02:52 +0200

    first commit

--- scripts/bd_to_prod.sh
+++ scripts/bd_to_prod.sh
@@ -21,27 +21,15 @@ npm i
 npm run build
 npm version patch
 cp package.json lib
-cd lib
-
 
 echo "Setting production ad NODE_ENV"
 export NODE_ENV=production
 
-echo "Creating new credential files"
-ls
-enc_location=../common-resources/encrypted_files/credentials_production.enc
-if [[ ! -f ${enc_location} ]]
-then
-    echo "$enc_location not found"
-    exit 1
-fi
-
-mkdir -p ~/.aws
-echo ${KEY} | gpg --batch -d --passphrase-fd 0 ${enc_location} > ~/.aws/credentials
-
 echo "Push to NPM"
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
+pushd lib
 npm run semantic-release
 
 echo \{\"type\":\"Release\",\"repo\":\"${CIRCLE_PROJECT_REPONAME}\",\"buildUrl\":\"${CIRCLE_BUILD_URL}\"\} | curl -X POST "https://listener.logz.io:8071?token=${LOGZ}" -v --data-binary @-
 git push origin master
+popd
