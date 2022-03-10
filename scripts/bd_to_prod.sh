76f4826d3bb29216e962ef9ffb1b846a97a9df12
CircleCI <no-reply@build.com>
2022-03-10 16:19:58 +0200

    fix: semantic

--- scripts/bd_to_prod.sh
+++ scripts/bd_to_prod.sh
@@ -1,26 +1,11 @@
-#!/usr/bin/env bash
-set -e
+echo "Creating new credential files"
+enc_location=../common-resources/encrypted_files/credentials_production.enc
+if [[ ! -f ${enc_location} ]]
+then
+    echo "$enc_location not found"
+    exit 1
+fi
 
-setup_git() {
-    git config --global user.email "no-reply@build.com"
-    git config --global user.name "CircleCI"
-    git checkout master
-}
-
-echo "Install a project with a clean state"
-npm ci
-
-echo "Build tracer"
-npm run build
-cp package.json lib
-setup_git
-
-echo "Setting production ad NODE_ENV"
-export NODE_ENV=production
-
-echo "Push to NPM"
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
-npm run semantic-release
-
-echo \{\"type\":\"Release\",\"repo\":\"${CIRCLE_PROJECT_REPONAME}\",\"buildUrl\":\"${CIRCLE_BUILD_URL}\"\} | curl -X POST "https://listener.logz.io:8071?token=${LOGZ}" -v --data-binary @-
-git push origin master
+mkdir -p ~/.aws
+echo "${KEY}" | gpg --batch -d --passphrase-fd 0 ${enc_location} > ~/.aws/credentials
+./scripts/deploy.sh --env prod
