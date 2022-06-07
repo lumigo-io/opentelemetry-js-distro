119d23d4152842a7d1c3bc6a344b2eb215be8907
CircleCI <no-reply@build.com>
2022-06-07 15:58:24 +0300

    chore: add versions matrix tests

--- scripts/install_integration.sh
+++ scripts/install_integration.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-
-echo "I AM HERE 1 " && pwd
 pushd src/test/integration/node
-echo "I AM HERE 2 " && pwd
 npm i
 popd
-echo "I AM HERE 3" && pwd
--- scripts/prepare_node_wrapper.sh
+++ scripts/prepare_node_wrapper.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 echo "clean"
-echo "I AM HERE " && pwd
-echo "i have here:  " && ls
 
 rm -rf dist wrapper.tgz || true
 echo "build"
@@ -10,4 +8,3 @@ npm ci
 npm run build
 tracer="$(npm pack)"
 mv $tracer wrapper.tgz
-echo "i have here 2:  " && ls
