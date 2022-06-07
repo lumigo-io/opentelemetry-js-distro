b9a1d46565721cdc842b6bcc08ee4bc9409b7218
CircleCI <no-reply@build.com>
2022-06-07 15:28:45 +0300

    chore: add versions matrix tests

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -2,10 +2,4 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/**/*.test.ts','**/**/*.test.js'],
   roots: ['./src/test/integration'],
-  coverageDirectory: './coverage/',
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '**/src/**/**/**/**.js',
-    '**/src/**/**/**/**.ts',
-  ],
 };
--- src/test/integration/express.test.ts
+++ src/test/integration/express.test.ts
@@ -13,7 +13,7 @@ describe('interface', function () {
       resolver = resolve;
     });
     app = await executeNpmScriptWithCallback(
-      './test/integration/node',
+      '.src/test/integration/node',
       (port) =>
         callContainer(port, 'chucknorris', 'get', {
           a: '1',
