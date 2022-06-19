bbac7b2a473143cbab849157b08570db70edbfae
“OrrLumigo” <orr@lumigo.com>
2022-06-19 15:35:43 +0300

    chore: 100% UT coverage

--- testUtils/jest.setup.js
+++ testUtils/jest.setup.js
@@ -1,11 +1,9 @@
 const oldEnv = Object.assign({}, process.env);
 
 beforeEach(() => {
- console.log("IN JEST SETUP beforeEach")
  process.env = { ...oldEnv };
 });
 
 afterEach(() => {
- console.log("IN JEST SETUP afterEach")
  process.env = { ...oldEnv };
  });
