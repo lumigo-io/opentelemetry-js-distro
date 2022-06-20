256a8f94d7b929d02429a8c46502c8fdf3e1d74d
Adam Fisher <adamf@lumigo.io>
2022-06-20 15:44:00 +0300

    RD-8364 simplify local component testing

--- src/test/component/node/installDependencies.js
+++ src/test/component/node/installDependencies.js
@@ -1,33 +1,32 @@
+const fs = require('fs');
 const {spawnSync} = require('child_process');
 
 console.log(`Installing supported dependency versions...`);
 
 const supportedDependencies = require('./package.json').lumigo.supportedDependencies;
 spawnSync('npm', ['install']);
-spawnSync('mkdir', ['./node_modules/.tmp']);
+fs.mkdirSync('./node_modules/.tmp', { recursive: true});
 
 for (const dependency in supportedDependencies) {
   // install each dependency version and move it to a holding location
   supportedDependencies[dependency].forEach((version) => {
-    console.log(`Installing ${dependency}@${version}`);
-    if (version) {
-      spawnSync('npm', ['install', `${dependency}@${version}`]);
-      spawnSync('mv', [`./node_modules/${dependency}`, `./node_modules/.tmp/${dependency}@${version}`]);
-    } else {
-      spawnSync('npm', ['install', `${dependency}`]);
-      spawnSync('mv', [`./node_modules/${dependency}`, `./node_modules/.tmp/${dependency}`]);
+    let fullName = version ? `${dependency}@${version}` : dependency;
+    let holdingPath = `./node_modules/.tmp/${fullName}`;
+    try {
+      fs.accessSync(holdingPath, fs.constants.F_OK);
+      console.log(`${fullName} installed`)
+    } catch (err) {
+      console.log(`Installing ${fullName}`);
+      spawnSync('npm', ['install', fullName]);
+      spawnSync('mv', [`./node_modules/${dependency}`, holdingPath]);
     }
   });
 
   // move each dependency version from its holding location to an active module directory
   supportedDependencies[dependency].forEach((version) => {
-    console.log(`Moving ${dependency}@${version}`);
-    if (version) {
-      spawnSync('mv', [`./node_modules/.tmp/${dependency}@${version}`, `./node_modules/${dependency}@${version}`]);
-    } else {
-      spawnSync('mv', [`./node_modules/.tmp/${dependency}`, `./node_modules/${dependency}`]);
-    }
+    let fullName = version ? `${dependency}@${version}` : dependency;
+    console.log(`Copying ${fullName}`);
+    spawnSync('cp', [`-a`, `./node_modules/.tmp/${fullName}`, `./node_modules/${fullName}`]);
   });
 }
-spawnSync('rm', ['-rf', './node_modules/.tmp']);
 console.log(`Supported dependency versions installed successfully.`);
