d7e9bd1525ef59b0943a10abec2d45b895983cae
“OrrLumigo” <orr@lumigo.com>
2022-07-05 15:28:58 +0300

    fix: clean resource attributes with detectors

--- src/wrapper.ts
+++ src/wrapper.ts
@@ -156,6 +156,7 @@ const trace = async (): Promise<void> => {
       const resourceAttributes = {
         framework: 'express',
         'process.environ': JSON.stringify(process.env),
+        envs: JSON.stringify(process.env),
       };
       if (metadata) Object.assign(resourceAttributes, { metadata });
       const config = {
