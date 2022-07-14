0e92abb532a93db7bf240ac129152f4db42b1f5b
“OrrLumigo” <orr@lumigo.com>
2022-07-14 19:47:01 +0300

    fix: clean resource attributes with detectors

--- test/component/express.compatibility.test.ts
+++ test/component/express.compatibility.test.ts
@@ -7,6 +7,145 @@ import { callContainer, executeNpmScriptWithCallback } from './helpers/helpers';
 import { instrumentationsVersionManager } from './helpers/InstrumentationsVersionManager';
 
 describe("'All Instrumentation's tests'", () => {
+  const spansResolvers = {
+    express: (path: string, resolver) => {
+      const allFileContents = fs.readFileSync(path, 'utf-8');
+      const lines = allFileContents.split(/\r?\n/).filter((l) => l !== '');
+      if (
+        lines.length === 3 &&
+        lines[0].startsWith('{"traceId"') &&
+        lines[1].startsWith('{"traceId"') &&
+        lines[2].startsWith('{"traceId"')
+      ) {
+        resolver(lines);
+      }
+    },
+  };
+
+  const tests = {
+    express: (spans: any[]) => {
+      expect(spans).toHaveLength(3);
+      const serverSpan = spans.find((span) => span.kind === 0);
+      const internalSpan = spans.find((span) => span.kind === 1);
+      const clientSpan = spans.find((span) => span.kind === 2);
+      expect(
+        serverSpan.traceId === internalSpan.traceId && serverSpan.traceId === clientSpan.traceId
+      ).toBeTruthy();
+
+      expect(serverSpan).toMatchObject({
+        traceId: expect.any(String),
+        parentId: expect.any(String),
+        name: 'GET /invoke-requests',
+        id: expect.any(String),
+        kind: 0,
+        timestamp: expect.any(Number),
+        duration: expect.any(Number),
+        resource: {
+          attributes: {
+            'service.name': 'express-js',
+            'telemetry.sdk.language': 'nodejs',
+            'telemetry.sdk.name': 'opentelemetry',
+            'telemetry.sdk.version': '1.1.1',
+            framework: 'express',
+            'process.environ': expect.stringMatching(/\{.*\}/),
+            'lumigo.distro.version': expect.stringMatching(/1\.\d+\.\d+/),
+            'process.pid': expect.any(Number),
+            'process.runtime.version': expect.stringMatching(/\d+\.\d+\.\d+/),
+            'process.runtime.name': 'nodejs',
+            'process.executable.name': 'node',
+          },
+        },
+        attributes: {
+          'http.method': 'GET',
+          'http.target': '/invoke-requests',
+          'http.flavor': '1.1',
+          'http.host': expect.stringMatching(/localhost:\d+/),
+          'http.scheme': 'http',
+          'net.peer.ip': '::ffff:127.0.0.1',
+          'http.request.query': '{}',
+          'http.request.headers': expect.stringMatching(/\{.*\}/),
+          'http.response.headers': expect.stringMatching(/\{.*\}/),
+          'http.response.body': expect.stringMatching(
+            /\["animal","career","celebrity","dev","explicit","fashion","food","history","money","movie","music","political","religion","science","sport","travel"\]/
+          ),
+          'http.request.body': '{}',
+          'http.route': '/invoke-requests',
+          'express.route.full': '/invoke-requests',
+          'express.route.configured': '/invoke-requests',
+          'express.route.params': '{}',
+          'http.status_code': 200,
+        },
+        status: {
+          code: 1,
+        },
+        events: [],
+      });
+
+      expect(internalSpan).toMatchObject({
+        traceId: expect.any(String),
+        id: expect.any(String),
+        timestamp: expect.any(Number),
+        duration: expect.any(Number),
+        name: 'HTTP GET',
+        kind: 1,
+        attributes: {
+          'http.url': expect.stringMatching(/http:\/\/localhost:\d+\/invoke-requests/),
+          'http.host': expect.stringMatching(/localhost:\d+/),
+          'net.host.name': 'localhost',
+          'http.method': 'GET',
+          'http.target': '/invoke-requests',
+          'http.user_agent': 'axios/0.21.4',
+          'http.flavor': '1.1',
+          'net.transport': 'ip_tcp',
+          'net.host.ip': '::ffff:127.0.0.1',
+          'net.host.port': expect.any(Number),
+          'net.peer.ip': '::ffff:127.0.0.1',
+          'net.peer.port': expect.any(Number),
+          'http.status_code': 200,
+          'http.status_text': 'OK',
+          'http.route': '/invoke-requests',
+        },
+        status: {
+          code: 0,
+        },
+        events: [],
+      });
+      expect(clientSpan).toMatchObject({
+        traceId: expect.any(String),
+        parentId: expect.any(String),
+        id: expect.any(String),
+        timestamp: expect.any(Number),
+        duration: expect.any(Number),
+        name: 'HTTPS GET',
+        kind: 2,
+        attributes: {
+          'http.url': 'https://api.chucknorris.io/jokes/categories',
+          'http.method': 'GET',
+          'http.target': '/jokes/categories',
+          'net.peer.name': 'api.chucknorris.io',
+          'http.request.body': '""',
+          'net.peer.ip': expect.stringMatching(
+            /\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b/
+          ),
+          'net.peer.port': 443,
+          'http.host': 'api.chucknorris.io:443',
+          'http.status_code': 200,
+          'http.status_text': 'OK',
+          'http.flavor': '1.1',
+          'http.request.headers': expect.stringMatching(/\{.*\}/),
+          'http.response.headers': expect.stringMatching(/\{.*\}/),
+          'http.response.body': expect.stringMatching(
+            /\["animal","career","celebrity","dev","explicit","fashion","food","history","money","movie","music","political","religion","science","sport","travel"\]/
+          ),
+        },
+        status: {
+          code: 0,
+        },
+        events: [],
+      });
+    },
+  };
+
   afterAll(() => {
     const versions = instrumentationsVersionManager.getInstrumantaionsVersions();
     Object.keys(versions).forEach((lib) => {
@@ -75,24 +214,10 @@ describe("'All Instrumentation's tests'", () => {
           const waitForDependencySpans = new Promise((resolve) => {
             resolver = resolve;
           });
-          const foundThreeSpans = (resolver: (value: unknown) => void, value: any) =>
-            resolver(value);
-          const spanCreatedHandler = (path: string) => {
-            const allFileContents = fs.readFileSync(path, 'utf-8');
-            const lines = allFileContents.split(/\r?\n/).filter((l) => l !== '');
-            if (
-              lines.length === 3 &&
-              lines[0].startsWith('{"traceId"') &&
-              lines[1].startsWith('{"traceId"') &&
-              lines[2].startsWith('{"traceId"')
-            ) {
-              foundThreeSpans(resolver, lines);
-            }
-          };
 
           watcher = watchDir(`${__dirname}/node/spans`, {
-            onAddFileEvent: spanCreatedHandler,
-            onChangeFileEvent: spanCreatedHandler,
+            onAddFileEvent: (path) => spansResolvers[dependency](path, resolver),
+            onChangeFileEvent: (path) => spansResolvers[dependency](path, resolver),
           });
 
           // sleep before running the container to make sure the watcher picks up all the changes
@@ -105,7 +230,7 @@ describe("'All Instrumentation's tests'", () => {
                 a: '1',
               }),
             () => {},
-            'start:injected',
+            `start:${dependency}:injected`,
             {
               LUMIGO_TRACER_TOKEN: 't_123321',
               LUMIGO_DEBUG_SPANDUMP: FILE_EXPORTER_FILE_NAME,
@@ -115,125 +240,7 @@ describe("'All Instrumentation's tests'", () => {
           );
           // @ts-ignore
           const spans = (await waitForDependencySpans).map((text) => JSON.parse(text));
-          expect(spans).toHaveLength(3);
-          const serverSpan = spans.find((span) => span.kind === 0);
-          const internalSpan = spans.find((span) => span.kind === 1);
-          const clientSpan = spans.find((span) => span.kind === 2);
-          expect(
-            serverSpan.traceId === internalSpan.traceId && serverSpan.traceId === clientSpan.traceId
-          ).toBeTruthy();
-
-          expect(serverSpan).toMatchObject({
-            traceId: expect.any(String),
-            parentId: expect.any(String),
-            name: 'GET /invoke-requests',
-            id: expect.any(String),
-            kind: 0,
-            timestamp: expect.any(Number),
-            duration: expect.any(Number),
-            resource: {
-              attributes: {
-                'service.name': 'express-js',
-                'telemetry.sdk.language': 'nodejs',
-                'telemetry.sdk.name': 'opentelemetry',
-                'telemetry.sdk.version': '1.1.1',
-                framework: 'express',
-                'process.environ': expect.stringMatching(/\{.*\}/),
-                'lumigo.distro.version': expect.stringMatching(/1\.\d+\.\d+/),
-                'process.pid': expect.any(Number),
-                'process.runtime.version': expect.stringMatching(/\d+\.\d+\.\d+/),
-                'process.runtime.name': 'nodejs',
-                'process.executable.name': 'node',
-              },
-            },
-            attributes: {
-              'http.method': 'GET',
-              'http.target': '/invoke-requests',
-              'http.flavor': '1.1',
-              'http.host': expect.stringMatching(/localhost:\d+/),
-              'http.scheme': 'http',
-              'net.peer.ip': '::ffff:127.0.0.1',
-              'http.request.query': '{}',
-              'http.request.headers': expect.stringMatching(/\{.*\}/),
-              'http.response.headers': expect.stringMatching(/\{.*\}/),
-              'http.response.body': expect.stringMatching(
-                /\["animal","career","celebrity","dev","explicit","fashion","food","history","money","movie","music","political","religion","science","sport","travel"\]/
-              ),
-              'http.request.body': '{}',
-              'http.route': '/invoke-requests',
-              'express.route.full': '/invoke-requests',
-              'express.route.configured': '/invoke-requests',
-              'express.route.params': '{}',
-              'http.status_code': 200,
-            },
-            status: {
-              code: 1,
-            },
-            events: [],
-          });
-
-          expect(internalSpan).toMatchObject({
-            traceId: expect.any(String),
-            id: expect.any(String),
-            timestamp: expect.any(Number),
-            duration: expect.any(Number),
-            name: 'HTTP GET',
-            kind: 1,
-            attributes: {
-              'http.url': expect.stringMatching(/http:\/\/localhost:\d+\/invoke-requests/),
-              'http.host': expect.stringMatching(/localhost:\d+/),
-              'net.host.name': 'localhost',
-              'http.method': 'GET',
-              'http.target': '/invoke-requests',
-              'http.user_agent': 'axios/0.21.4',
-              'http.flavor': '1.1',
-              'net.transport': 'ip_tcp',
-              'net.host.ip': '::ffff:127.0.0.1',
-              'net.host.port': expect.any(Number),
-              'net.peer.ip': '::ffff:127.0.0.1',
-              'net.peer.port': expect.any(Number),
-              'http.status_code': 200,
-              'http.status_text': 'OK',
-              'http.route': '/invoke-requests',
-            },
-            status: {
-              code: 0,
-            },
-            events: [],
-          });
-          expect(clientSpan).toMatchObject({
-            traceId: expect.any(String),
-            parentId: expect.any(String),
-            id: expect.any(String),
-            timestamp: expect.any(Number),
-            duration: expect.any(Number),
-            name: 'HTTPS GET',
-            kind: 2,
-            attributes: {
-              'http.url': 'https://api.chucknorris.io/jokes/categories',
-              'http.method': 'GET',
-              'http.target': '/jokes/categories',
-              'net.peer.name': 'api.chucknorris.io',
-              'http.request.body': '""',
-              'net.peer.ip': expect.stringMatching(
-                /\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b/
-              ),
-              'net.peer.port': 443,
-              'http.host': 'api.chucknorris.io:443',
-              'http.status_code': 200,
-              'http.status_text': 'OK',
-              'http.flavor': '1.1',
-              'http.request.headers': expect.stringMatching(/\{.*\}/),
-              'http.response.headers': expect.stringMatching(/\{.*\}/),
-              'http.response.body': expect.stringMatching(
-                /\["animal","career","celebrity","dev","explicit","fashion","food","history","money","movie","music","political","religion","science","sport","travel"\]/
-              ),
-            },
-            status: {
-              code: 0,
-            },
-            events: [],
-          });
+          tests[dependency](spans);
           lastTest.failed = false;
         });
       }
