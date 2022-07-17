09532fdcaafc7ea16707dbf2b729764e9765677b
“OrrLumigo” <orr@lumigo.com>
2022-07-17 12:12:46 +0300

    fix: clean resource attributes with detectors

--- test/component/node/expressApp.js
+++ test/component/node/expressApp.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const express = require("express");
+const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -9,27 +9,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-const init = () => {
-  app.get('/invoke-requests', async (req, res) => {
-    const response = await axios.get('https://api.chucknorris.io/jokes/categories', {
-      headers: {
-        header: 'a',
-      },
-    });
-    res.send(response.data).status(200);
+app.get('/invoke-requests', async (req, res) => {
+  const response = await axios.get('https://api.chucknorris.io/jokes/categories', {
+    headers: {
+      header: 'a',
+    },
   });
+  res.send(response.data).status(200);
+});
 
-  const server = app.listen(0, () => {
-    const port = server.address().port;
-    console.log('Listening on port ' + port);
-    if (process.send) {
-      process.send(port);
-    } else {
-      console.log(`PORT:${port}`);
-    }
-  });
-};
-
-module.exports = {
-  init,
-};
+const server = app.listen(0, () => {
+  const port = server.address().port;
+  console.log('Listening on port ' + port);
+  if (process.send) {
+    process.send(port);
+  } else {
+    console.log(`PORT:${port}`);
+  }
+});
