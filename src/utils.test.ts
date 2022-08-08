352a4e1c217af0f0f5a69c288dbfef08e7b2bc38
OrrLumigo <orr@lumigo.com>
2022-08-08 12:11:12 +0300

    fix: semantic deploy

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,6 +1,44 @@
-import { extractEnvVars } from './utils';
+import { extractEnvVars, logger, safeRequire } from './utils';
+import http from 'http';
 
 describe('utils tests', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('safeRequire -> simple flow', () => {
+    const http = require('http');
+
+    const result = safeRequire('http');
+
+    expect(http).toEqual(result);
+  });
+
+  test('safeRequire -> simple flow', () => {
+    delete process.env.NODE_PATH;
+    jest.spyOn(logger, 'warn');
+    const result = safeRequire('BlaBlaBlaBla');
+
+    expect(result).toEqual(undefined);
+    expect(logger.warn).toBeCalledTimes(0);
+  });
+
+  test('safeRequire -> not exist', () => {
+    const result = safeRequire('BlaBlaBlaBla');
+
+    expect(result).toBeFalsy();
+  });
+
+  test('safeRequire -> other error', () => {
+    jest.doMock('fs', () => {
+      throw Error('RandomError');
+    });
+
+    const result = safeRequire('fs');
+
+    expect(result).toBeFalsy();
+  });
+
   describe('when evn vars are less then max size', () => {
     it('extractEnvVars should not filter anything', () => {
       process.env = {
