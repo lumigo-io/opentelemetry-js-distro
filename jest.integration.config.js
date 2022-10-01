4991a6346fdb497fa16ba6ba35d245764d320150
shanishiri <shani@lumigo.io>
2022-10-01 21:17:37 +0300

    IT infra refactor

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/integration/**/*.test.js'],
-  // testMatch: ['**/test/integration/express/express.test.js'],
   roots: ['./test'],
   setupFilesAfterEnv: ['./jest.integration.setup.js', "jest-json"],
-  // setupTestFrameworkScriptFile: "jest-json",
 };
