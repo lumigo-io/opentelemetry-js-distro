d6de4eab2b0c65f0254d4531e4dbd6df43c9726b
CircleCI <no-reply@build.com>
2022-10-18 14:22:29 +0300

    run checks from circleci

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-if [[ -n "$CI" ]]
+if [[ -n "$CIRCLECI" ]]
 then
     # Check if branch contains RD ticket value.
-    echo "$GITHUB_REF" | grep -E "[RDrd]-[0-9]+|master" || { echo "Please create a relevent ticket in Jira and connect it to this branch. Use jiranch." ; exit 1; }
+    echo "$CIRCLE_BRANCH" | grep -E "[RDrd]-[0-9]+|master" || { echo "Please create a relevent ticket in Jira and connect it to this branch. Use jiranch." ; exit 1; }
 fi
-
-npm run prepublishOnly
