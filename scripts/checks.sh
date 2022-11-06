314ed92ad4fad464257edf805a22762e65157b45
shanishiri <shani@lumigo.io>
2022-11-06 11:37:25 +0200

    Rd 9461 containers support execution tags (#117) [skipci]

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -3,5 +3,5 @@
 if [[ -n "$CIRCLECI" ]]
 then
     # Check if branch contains RD ticket value.
-    echo "$CIRCLE_BRANCH" | grep -E "[RDrd]-[0-9]+|master|version-testing-[0-9]{4}[0-9]{2}[0-9]{2}" || { echo "Please create a relevent ticket in Jira and connect it to this branch. Use jiranch." ; exit 1; }
+    echo "$CIRCLE_BRANCH" | grep -E "[RDrd]-[0-9]+|master|version-testing-[0-9]{4}[0-9]{2}[0-9]{2}|dependabot/" || { echo "Please create a relevent ticket in Jira and connect it to this branch. Use jiranch." ; exit 1; }
 fi
