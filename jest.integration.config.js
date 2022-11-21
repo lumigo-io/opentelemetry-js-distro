7c6ff9fba21d54e2c4b0d2afcc6873647072bc76
shanishiri <shani@lumigo.io>
2022-11-21 15:25:13 +0200

    RD-9643

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -3,9 +3,4 @@ module.exports = {
   testMatch: ['**/integration/**/*.test.js'],
   roots: ['./test'],
   setupFilesAfterEnv: ['./jest.integration.setup.js', "jest-json"],
-  reporters: [
-    "default",
-    "jest-github-actions-reporter"
-  ],
-  testLocationInResults: true
 };
