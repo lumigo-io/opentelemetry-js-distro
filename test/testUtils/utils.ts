0dfb716840bfe67fc1a1c5b8cbc4e94947aabfde
shanishiri <shani@lumigo.io>
2022-11-21 16:30:48 +0200

    RD-9643

--- test/testUtils/utils.ts
+++ test/testUtils/utils.ts
@@ -47,11 +47,10 @@ export function getStartedApp(serverFolder: string, serviceName: string, fileExp
         });
 
         app.on('exit', function (code, signal) {
-            console.info('app exited with ' +
-                `code: ${code} and signal: ${signal}`);
+            console.info(`app with pid: ${this.pid} exited with code: ${code} and signal: ${signal}`);
             //we kill the app with "SIGHUP" in the afterEach, we want to throw error only when it's real app issue
             if (signal !== "SIGHUP"){
-                throw new Error('app exit unexpectedly!');
+                throw new Error(`app with pid: ${this.pid} exit unexpectedly!`);
             }
         });
 
