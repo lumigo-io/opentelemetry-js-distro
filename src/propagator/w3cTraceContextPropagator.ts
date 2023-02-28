a4202622053c71c88653585c26c8a509dc5fb469
Michele Mancioppi <michelem@lumigo.io>
2023-02-28 15:37:12 +0100

    fix: make eslint happy

--- src/propagator/w3cTraceContextPropagator.ts
+++ src/propagator/w3cTraceContextPropagator.ts
@@ -14,7 +14,7 @@ const contextKeysSkipInject = [
 export class LumigoW3CTraceContextPropagator extends W3CTraceContextPropagator {
   override inject(context: Context, carrier: unknown, setter: TextMapSetter): void {
     if (typeof carrier === 'object') {
-      for (var key of contextKeysSkipInject) {
+      for (const key of contextKeysSkipInject) {
         if (key in carrier) {
           diag.debug(`Skipping injection of trace context due to key '${key}' in carrier`);
           return;
