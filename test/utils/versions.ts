61ae81ed79c0b91700d3a39f5ab490408ef89dd4
Adam Fisher <adamf@lumigo.io>
2023-08-15 16:51:18 +0300

    fix: untested unsupported versions preserved

--- test/utils/versions.ts
+++ test/utils/versions.ts
@@ -1,10 +1,25 @@
 import { readFileSync } from 'fs';
 import { dirname } from 'path';
+import { instrumentationsVersionManager } from '../helpers/InstrumentationsVersionManager';
 
 export function versionsToTest(instrumentationName: string, packageName: string) {
-    return readFileSync(`${dirname(dirname(__dirname))}/src/instrumentations/${instrumentationName}/tested_versions/${packageName}`)
-        .toString()
-        .split('\n')
-        .filter(Boolean)
-        .filter(version => !version.startsWith('!'));
+  const allVersions = readFileSync(
+    `${dirname(
+      dirname(__dirname)
+    )}/src/instrumentations/${instrumentationName}/tested_versions/${packageName}`
+  )
+    .toString()
+    .split('\n')
+    .filter(Boolean);
+  const supportedVersions = allVersions.filter((version) => !version.startsWith('!'));
+  const unsupportedVersions = allVersions
+    .filter((version) => version.startsWith('!'))
+    .map((version) => {
+      // strip ! prefix from version
+      return version.slice(1);
+    });
+  unsupportedVersions.forEach((version) => {
+    instrumentationsVersionManager.addPackageUnsupportedVersion(packageName, version);
+  });
+  return supportedVersions;
 }
