00e643cdbc16e4913dea5577ccd0f52ac3bafff2
Adam Fisher <adamf@lumigo.io>
2023-08-20 10:58:59 +0300

    chore: implement PR feedback

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,23 +18,16 @@ npm run build
 cp package.json dist
 setup_git
 
-echo "Dry-run of semantic-version to determine whether the package will be published"
+echo "Publish to NPM"
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
+echo "Running semantic-release"
 # merge stderr and stdout and check for "no new version is released". If found, bail.
-# don't forget to capture the grep failure with `|| echo ""` so that the script doesn't fail
-# when the line isn't found
-npm run semantic-release -- -d 2>&1 | grep "no new version is released" > no_new_version.txt || echo "It looks like a new version will be released!"
+npm run semantic-release 2>&1 | tee semantic_release_logs.txt
+cat semantic_release_logs.txt | grep "no new version is released" > no_new_version.txt || echo "It looks like a new version has been published!"
 if [ -s no_new_version.txt ]; then
-    # run the dry run a second time so that the output is printed to the console
-    echo npm run semantic-release -- -d
-    echo ""
     echo "Marking deployment as failed as no new version would be released."
     exit 1
 fi
-
-echo "Push to NPM"
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
-echo "Running semantic-release"
-npm run semantic-release
 
 echo "Pushing to origin/main"
 git push origin main
