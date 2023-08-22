3557235ccf63ac22d235f1b063cd3d29d7b0f39a
Ken Finnigan <ken@kenfinnigan.me>
2023-08-22 10:21:06 -0400

    Update checks per Adam's request

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -2,8 +2,7 @@
 
 if [[ -n "$CIRCLECI" ]]
 then
-    # Check if branch contains RD ticket value.
-    echo "$CIRCLE_BRANCH" | grep -E "[RDrd]-[0-9]+|master|version-testing-[0-9]{4}[0-9]{2}[0-9]{2}|dependabot/" || { echo "Please create a relevent ticket in Jira and connect it to this branch. Use jiranch." ; exit 1; }
+    npm run prettier:ci
 else
     npm run prettier:fix
 fi
