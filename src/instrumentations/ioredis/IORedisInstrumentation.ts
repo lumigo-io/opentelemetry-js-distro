c822a608c3ac51b3122a79202909272f108b23d5
Adam Fisher <adamf@lumigo.io>
2023-09-11 13:57:42 +0300

    feat: add ioredis support

---
+++ src/instrumentations/ioredis/IORedisInstrumentation.ts
@@ -0,0 +1,39 @@
+import { CommonUtils, ScrubContext } from '@lumigo/node-core';
+import { Span } from '@opentelemetry/api';
+import { IORedisInstrumentation } from '@opentelemetry/instrumentation-ioredis';
+import { getSpanAttributeMaxLength } from '../../utils';
+import { Instrumentor } from '../instrumentor';
+
+export default class LumigoIORedisInstrumentation extends Instrumentor<IORedisInstrumentation> {
+  getInstrumentedModule(): string {
+    return 'ioredis';
+  }
+
+  getInstrumentation(): IORedisInstrumentation {
+    return new IORedisInstrumentation({
+      dbStatementSerializer: function (cmdName, cmdArgs) {
+        const statement = [cmdName, ...cmdArgs].join(' ');
+        return CommonUtils.payloadStringify(
+          statement,
+          ScrubContext.HTTP_REQUEST_BODY,
+          getSpanAttributeMaxLength()
+        );
+      },
+      responseHook: (
+        span: Span,
+        cmdName: string,
+        cmdArgs: (string | Buffer)[],
+        response: unknown
+      ) => {
+        span.setAttribute(
+          `redis.response.body`,
+          CommonUtils.payloadStringify(
+            response?.toString(),
+            ScrubContext.HTTP_RESPONSE_BODY,
+            getSpanAttributeMaxLength()
+          )
+        );
+      },
+    });
+  }
+}
