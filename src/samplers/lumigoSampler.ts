685cb041839348cd696da6d57b7490cd4319256b
Sagiv Oulu <sagivo@lumigo.io>
2023-12-06 14:13:50 +0200

    chore: fix linting errors

--- src/samplers/lumigoSampler.ts
+++ src/samplers/lumigoSampler.ts
@@ -1,7 +1,6 @@
 import { Sampler, ParentBasedSampler } from '@opentelemetry/sdk-trace-base';
 import { Context, Link, Attributes, SpanKind } from '@opentelemetry/api';
 import { SamplingResult, SamplingDecision } from '@opentelemetry/sdk-trace-base';
-import {stringify} from "querystring";
 
 export class LumigoSampler implements Sampler {
   shouldSample(
@@ -22,12 +21,14 @@ export class LumigoSampler implements Sampler {
       links,
     });
 
-    let decision = SamplingDecision.RECORD_AND_SAMPLED
+    let decision = SamplingDecision.RECORD_AND_SAMPLED;
     const url = extractUrl(attributes);
-    console.log('url', url)
+    console.log('url', url);
     if (url && shouldSkipSpanOnRouteMatch(url)) {
-      console.debug(`Dropping trace for url '${url} because it matches the auth-filter regex specified by 'LUMIGO_FILTER_HTTP_ENDPOINTS_REGEX'`)
-      decision = SamplingDecision.NOT_RECORD
+      console.debug(
+        `Dropping trace for url '${url} because it matches the auth-filter regex specified by 'LUMIGO_FILTER_HTTP_ENDPOINTS_REGEX'`
+      );
+      decision = SamplingDecision.NOT_RECORD;
     }
 
     return { decision: decision };
@@ -36,49 +37,55 @@ export class LumigoSampler implements Sampler {
 
 export const extractUrl = (attributes: Attributes): string | null => {
   if (!attributes) {
-    return null
+    return null;
   }
 
   // Try building a raw url from given attributes
-  let raw_url = null
+  let raw_url = null;
   if (attributes['http.url']) {
-    raw_url = attributes['http.url'].toString()
+    raw_url = attributes['http.url'].toString();
   }
-  if (!raw_url && attributes['http.scheme'] && attributes['http.host'] && attributes['http.target']) {
-    raw_url = `${attributes['http.scheme']}://${attributes['http.host']}${attributes['http.target']}`
+  if (
+    !raw_url &&
+    attributes['http.scheme'] &&
+    attributes['http.host'] &&
+    attributes['http.target']
+  ) {
+    raw_url = `${attributes['http.scheme']}://${attributes['http.host']}${attributes['http.target']}`;
   }
 
   if (!raw_url) {
-    return null
+    return null;
   }
 
   const parsedUrl = new URL(raw_url);
   const path = parsedUrl.pathname && parsedUrl.pathname !== '/' ? parsedUrl.pathname : '';
   return `${parsedUrl.protocol}//${parsedUrl.host}${path}${parsedUrl.search}`;
-}
+};
 
 export const shouldSkipSpanOnRouteMatch = (url: string): boolean => {
   if (!url) {
-    return false
+    return false;
   }
 
   if (!process.env.LUMIGO_FILTER_HTTP_ENDPOINTS_REGEX) {
-    return false
+    return false;
   }
 
   if (process.env.LUMIGO_FILTER_HTTP_ENDPOINTS_REGEX) {
-    let regex: null | RegExp = null
+    let regex: null | RegExp = null;
     try {
-      regex = new RegExp(process.env.LUMIGO_FILTER_HTTP_ENDPOINTS_REGEX)
-      return regex.test(url)
-    }
-    catch (err) {
-        console.error(`Invalid regex in 'LUMIGO_FILTER_HTTP_ENDPOINTS_REGEX': '${process.env.LUMIGO_FILTER_HTTP_ENDPOINTS_REGEX}'`)
-        return false
+      regex = new RegExp(process.env.LUMIGO_FILTER_HTTP_ENDPOINTS_REGEX);
+      return regex.test(url);
+    } catch (err) {
+      console.error(
+        `Invalid regex in 'LUMIGO_FILTER_HTTP_ENDPOINTS_REGEX': '${process.env.LUMIGO_FILTER_HTTP_ENDPOINTS_REGEX}'`
+      );
+      return false;
     }
   }
-  return false
-}
+  return false;
+};
 
 export const getLumigoSampler = () => {
   const lumigoSampler = new LumigoSampler();
