1d68bb7dcbb640a7f31d20e164b0fea40c740eb3
nadav <nadavg@lumigo.io>
2024-01-23 15:16:05 +0200

    wip

--- test/utils/versions.ts
+++ test/utils/versions.ts
@@ -1,6 +1,7 @@
 import { readFileSync } from 'fs';
 import { dirname } from 'path';
 import { instrumentationsVersionManager } from '../helpers/InstrumentationsVersionManager';
+import {runOneTimeWrapper} from "@lumigo/node-core/lib/common";
 
 const VERSION_UNDER_TEST =
   process.env.INSTRUMENTATION_UNDER_TEST &&
@@ -11,13 +12,14 @@ const VERSION_UNDER_TEST =
     : undefined;
 
 export function versionsToTest(instrumentationName: string, packageName: string) {
+  const runtime = process.version;
   if (VERSION_UNDER_TEST) {
     return [VERSION_UNDER_TEST];
   }
   const allVersions = readFileSync(
     `${dirname(
       dirname(__dirname)
-    )}/src/instrumentations/${instrumentationName}/tested_versions/${packageName}`
+    )}/src/instrumentations/${instrumentationName}/tested_versions/${runtime}/${packageName}`
   )
     .toString()
     .split('\n')
