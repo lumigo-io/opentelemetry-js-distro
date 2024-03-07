ec258ee87c3a6c577eddfddd14a0e49c798b7ae5
Harel Moshe <harelm@lumigo.io>
2024-03-07 13:53:28 +0200

    Version testing setup

--- jest.instrumentations.config.js
+++ jest.instrumentations.config.js
@@ -4,7 +4,7 @@ const instrumentationToTest = process.env.INSTRUMENTATION_UNDER_TEST || allInstr
 /** @type {import('jest').Config} */
 const config = {
   testEnvironment: 'node',
-  testMatch: [`**/instrumentations/${instrumentationToTest}/*.test.ts`],
+  testMatch: [`**/instrumentations/${instrumentationToTest}/**/*.test.ts`],
   roots: ['./test'],
   setupFilesAfterEnv: ['./jest.instrumentations.setup.js', 'jest-json', 'jest-expect-message', 'jest-extended/all'],
   reporters: ['default', 'jest-summarizing-reporter'],
