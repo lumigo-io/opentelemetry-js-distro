7b938142280c821797f244bd18bb49a09ea6443b
Guy Moses <guy@lumigo.io>
2024-05-01 19:43:03 +0300

    feat: allow disabling pg automatic instrumentation through env-var

--- src/instrumentations/pg/PgInstrumentation.test.js
+++ src/instrumentations/pg/PgInstrumentation.test.js
@@ -1,11 +1,26 @@
 import LumigoPgInstrumentation from './PgInstrumentation';
+import child_process from 'child_process';
 
 describe('LumigoPgInstrumentation', () => {
+  const oldEnv = Object.assign({}, process.env);
+  beforeEach(() => {
+    process.env = { ...oldEnv };
+  });
+
   afterEach(() => {
     jest.clearAllMocks();
+    process.env = { ...oldEnv };
   });
 
   let lumigoPgInstrumentation = new LumigoPgInstrumentation();
+
+  test('disable pg instrumentation', () => {
+    const child_process = require('child_process');
+    child_process.execSync('npm install pg', { stdio: 'inherit' });
+
+    process.env.LUMIGO_DISABLE_PG_INSTRUMENTATION = 'true';
+    expect(lumigoPgInstrumentation.isApplicable()).toEqual(false);
+  });
 
   test('getInstrumentedModule should return "pg"', () => {
     expect(lumigoPgInstrumentation.getInstrumentedModule()).toEqual('pg');
