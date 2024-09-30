1fe5aa2de92c1ab84dfaa288ed1cf53d390721dd
Eugene Orlovsky <eugene.orlovsky@staircase.co>
2024-09-30 12:10:00 +0200

    init,

---
+++ test/instrumentations/next/app/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1>Hello World!</h1>
+    </div>
+  )
+}
