b60288560ed04a210a14068f16e9cfabd69cf365
Eugene Orlovsky <eugene.orlovsky@staircase.co>
2024-10-01 14:26:16 +0200

    init,

---
+++ src/instrumentations/next/NextInstrumentation.ts
@@ -0,0 +1,19 @@
+import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
+import { TracingInstrumentor } from '../instrumentor';
+
+export default class LumigoNestInstrumentation extends TracingInstrumentor<NestInstrumentation> {
+  override isApplicable(): boolean {
+    return (
+      super.isApplicable() &&
+      process.env.LUMIGO_DISABLE_NEST_INSTRUMENTATION?.toLocaleLowerCase() !== 'true'
+    );
+  }
+
+  getInstrumentedModule(): string {
+    return '@nestjs/core';
+  }
+
+  getInstrumentation(): NestInstrumentation {
+    return new NestInstrumentation();
+  }
+}
