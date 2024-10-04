6fae162462324bf766b790f375f5f936fd6a356e
Eugene Orlovsky <eugene.orlovsky@staircase.co>
2024-10-04 16:12:55 +0200

    mongo tests fix,

--- test/instrumentations/mongodb/mongodbTestUtils.js
+++ test/instrumentations/mongodb/mongodbTestUtils.js
@@ -9,8 +9,10 @@ export function getExpectedResourceAttributes() {
     'framework': expect.toBeOneOf(['node', 'express']),
     'process.environ': expect.any(String),
     'lumigo.distro.version': expect.stringMatching(/1\.\d+\.\d+/),
-    'process.pid': expect.any(Number),
     'process.executable.name': 'node',
+    'process.executable.path': expect.any(String),
+    'process.owner': expect.any(String),
+    'process.pid': expect.any(Number),
     'process.runtime.description': 'Node.js',
     'process.runtime.name': 'nodejs',
     'process.runtime.version': expect.stringMatching(/\d+\.\d+\.\d+/),
@@ -21,17 +23,24 @@ export function getExpectedSpan(nameSpanAttr, resourceAttributes, dbStatement) {
   return {
     traceId: expect.any(String),
     id: expect.any(String),
+    parentId: expect.any(String),
     timestamp: expect.any(Number),
     duration: expect.any(Number),
     name: nameSpanAttr,
     kind: SpanKind.CLIENT,
     resource: {
-      attributes: resourceAttributes,
+      _attributes: resourceAttributes,
+      asyncAttributesPending: expect.any(Boolean),
+      _syncAttributes: expect.any(Object),
     },
     attributes: {
       'db.system': 'mongodb',
       'db.name': 'myProject',
       'db.mongodb.collection': 'insertOne',
+      "db.operation": "insert",
+      "db.connection_string": expect.any(String),
+      "net.peer.name": expect.any(String),
+      "net.peer.port": expect.any(String),
       'db.statement': dbStatement,
     },
     status: {
@@ -56,7 +65,7 @@ export function getExpectedSpanWithParent(
     name: nameSpanAttr,
     kind: SpanKind.CLIENT,
     resource: {
-      attributes: resourceAttributes,
+      _attributes: resourceAttributes,
     },
     attributes: {
       'db.system': 'mongodb',
