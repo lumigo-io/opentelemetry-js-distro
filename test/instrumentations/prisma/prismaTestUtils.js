8348a51e94638b9685ce588d4b82330a03b04074
Eugene Orlovsky <eugene.orlovsky@staircase.co>
2024-10-04 16:34:36 +0200

    prisma tests fix,

--- test/instrumentations/prisma/prismaTestUtils.js
+++ test/instrumentations/prisma/prismaTestUtils.js
@@ -7,14 +7,18 @@ export function getExpectedResourceAttributes() {
     'telemetry.sdk.language': 'nodejs',
     'telemetry.sdk.name': 'opentelemetry',
     'telemetry.sdk.version': expect.any(String),
-    'framework': expect.toBeOneOf(['node', 'express']),
+    'framework': expect.toBeOneOf(['node', 'express'] ),
     'process.environ': expect.any(String),
     'lumigo.distro.version': expect.stringMatching(/1\.\d+\.\d+/),
     'process.pid': expect.any(Number),
     'process.executable.name': 'node',
+    'process.executable.path': expect.any(String),
+    'process.command_args': expect.any(Array),
     'process.runtime.description': 'Node.js',
     'process.runtime.name': 'nodejs',
     'process.runtime.version': expect.stringMatching(/\d+\.\d+\.\d+/),
+    'process.command': expect.any(String),
+    'process.owner': expect.any(String),
   };
 }
 
@@ -22,12 +26,15 @@ export function getExpectedSpan({ name, resourceAttributes, attributes }) {
   return {
     traceId: expect.any(String),
     id: expect.any(String),
+    parentId: expect.any(String),
     timestamp: expect.any(Number),
     duration: expect.any(Number),
     name: name,
     kind: SpanKind.INTERNAL,
     resource: {
-      attributes: resourceAttributes,
+      _attributes: resourceAttributes,
+      asyncAttributesPending: expect.any(Boolean),
+      _syncAttributes: expect.any(Object),
     },
     attributes: attributes,
     status: {
