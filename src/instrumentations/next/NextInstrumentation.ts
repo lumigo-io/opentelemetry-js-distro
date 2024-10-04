fe3fffb8132835624d6402d8b3ff2d8ee9bbd781
Eugene Orlovsky <eugene.orlovsky@staircase.co>
2024-10-04 13:50:42 +0200

    code review,

--- src/instrumentations/next/NextInstrumentation.ts
+++ src/instrumentations/next/NextInstrumentation.ts
@@ -3,13 +3,6 @@ import { FetchInstrumentation } from '@vercel/otel';
 import { TracingInstrumentor } from '../instrumentor';
 
 export default class LumigoNestInstrumentation extends TracingInstrumentor<FetchInstrumentation> {
-  override isApplicable(): boolean {
-    return (
-      super.isApplicable() &&
-      process.env.LUMIGO_DISABLE_NEST_INSTRUMENTATION?.toLocaleLowerCase() !== 'true'
-    );
-  }
-
   getInstrumentedModule(): string {
     return 'next';
   }
