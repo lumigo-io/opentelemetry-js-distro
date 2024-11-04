0364cbac21b3bcf083c1db3d8065f52dc81da048
CircleCI <no-reply@build.com>
2024-11-04 11:02:40 +0200

    refactor

--- src/distro-sync.ts
+++ src/distro-sync.ts
@@ -1,15 +1,10 @@
 import { init } from "./distro"
 import deasync from "deasync";
 
-let lumigoSdkInitResult = null
+let done = false
 
-init.then((result) => {
-  lumigoSdkInitResult = result
+init.then(() => {
+  done = true
 })
 
-while (lumigoSdkInitResult === null) {
-  deasync.sleep(100); // This will block, but allow the event loop to process the Promise resolution
-  console.log("Waiting for Lumigo SDK initialization to complete...")
-}
-
-console.log("Synchronous Lumigo SDK initialization completed.")
+deasync.loopWhile(() => !done)
