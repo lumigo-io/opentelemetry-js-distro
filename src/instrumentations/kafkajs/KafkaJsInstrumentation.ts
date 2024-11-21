dbf7a3e7259c07f7dd293aecb4545f2f2fba9736
CircleCI <no-reply@build.com>
2024-11-21 14:17:56 +0200

    chore: move to @opentelemetry kafka instrumetation

--- src/instrumentations/kafkajs/KafkaJsInstrumentation.ts
+++ src/instrumentations/kafkajs/KafkaJsInstrumentation.ts
@@ -1,7 +1,10 @@
 import { CommonUtils, ScrubContext } from '@lumigo/node-core';
 import type { Span } from '@opentelemetry/api';
-import type { Message } from 'kafkajs';
-import { KafkaJsInstrumentation } from 'opentelemetry-instrumentation-kafkajs';
+import {
+  MessagingDestinationKindValues,
+  SemanticAttributes,
+} from '@opentelemetry/semantic-conventions';
+import { KafkaJsInstrumentation, MessageInfo } from '@opentelemetry/instrumentation-kafkajs';
 import { getSpanAttributeMaxLength } from '../../utils';
 import { TracingInstrumentor } from '../instrumentor';
 
@@ -12,24 +15,32 @@ export default class LumigoKafkaJsInstrumentation extends TracingInstrumentor<Ka
 
   getInstrumentation(): KafkaJsInstrumentation {
     return new KafkaJsInstrumentation({
-      producerHook: (span: Span, topic: string, message: Message) => {
+      producerHook: (span: Span, messageInfo: MessageInfo) => {
         span.setAttribute(
           'messaging.produce.body',
           CommonUtils.payloadStringify(
-            message.value.toString(),
+            messageInfo.message.value.toString(),
             ScrubContext.HTTP_REQUEST_QUERY,
             getSpanAttributeMaxLength()
           )
         );
+        span.setAttribute(
+          SemanticAttributes.MESSAGING_DESTINATION_KIND,
+          MessagingDestinationKindValues.TOPIC
+        );
       },
-      consumerHook: (span: Span, topic: string, message: Message) => {
+      consumerHook: (span: Span, messageInfo: MessageInfo) => {
         span.setAttribute(
           'messaging.consume.body',
           CommonUtils.payloadStringify(
-            message.value.toString(),
+            messageInfo.message.value.toString(),
             ScrubContext.HTTP_RESPONSE_BODY,
             getSpanAttributeMaxLength()
           )
+        );
+        span.setAttribute(
+          SemanticAttributes.MESSAGING_DESTINATION_KIND,
+          MessagingDestinationKindValues.TOPIC
         );
       },
     });
