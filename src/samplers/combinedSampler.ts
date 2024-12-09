00055ab858c8f539248a378f0b1d923f61e761c6
Eugene Orlovsky <eugeneo@lumigo.io>
2024-12-09 14:55:43 +0100

    feat: RD-14190-Redis-and-Mongo-reduced-instrumentation (#554)
    * feat: mongodg reduced 
    * feat: redisSampler

---
+++ src/samplers/combinedSampler.ts
@@ -0,0 +1,54 @@
+import {
+  Sampler,
+  SamplingResult,
+  SamplingDecision,
+  ParentBasedSampler,
+} from '@opentelemetry/sdk-trace-base';
+import type { Context, Link, Attributes, SpanKind } from '@opentelemetry/api';
+
+import { LumigoSampler } from './lumigoSampler';
+import { MongodbSampler } from './mongodbSampler';
+import { RedisSampler } from './redisSampler';
+
+export class CombinedSampler implements Sampler {
+  private samplers: Sampler[];
+
+  constructor(...samplers: Sampler[]) {
+    this.samplers = samplers;
+  }
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  shouldSample(
+    context: Context,
+    traceId: string,
+    spanName: string,
+    spanKind: SpanKind,
+    attributes: Attributes,
+    links: Link[]
+  ): SamplingResult {
+    // Iterate through each sampler
+    for (const sampler of this.samplers) {
+      const result = sampler.shouldSample(context, traceId, spanName, spanKind, attributes, links);
+
+      // If any sampler decides NOT_RECORD, we respect that decision
+      if (result.decision === SamplingDecision.NOT_RECORD) {
+        return result;
+      }
+    }
+
+    // If none decided to NOT_RECORD, we default to RECORD_AND_SAMPLED
+    return { decision: SamplingDecision.RECORD_AND_SAMPLED };
+  }
+}
+
+export const getCombinedSampler = () => {
+  const lumigoSampler = new LumigoSampler();
+  const mongodbSampler = new MongodbSampler();
+  const redisSampler = new RedisSampler();
+  const combinedSampler = new CombinedSampler(lumigoSampler, mongodbSampler, redisSampler);
+
+  return new ParentBasedSampler({
+    root: combinedSampler,
+    remoteParentSampled: combinedSampler,
+    localParentSampled: combinedSampler,
+  });
+};
